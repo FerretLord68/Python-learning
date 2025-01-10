5bc7e6a32b6d80f9de0ecbd448e479147f3d378c
Frederik Juul Olsen <bambiergratis@gmail.com>
2025-01-10 13:39:06 +0100

    Get sys info

--- server_automation.py
+++ server_automation.py
@@ -1,4 +1,5 @@
-# In this script I'll tuch opon Display System Information, 
+# In this script I'll tuch opon 
+# Display System Information, 
 # Manage Services (Start, Stop, Restart),
 # Monitor and Log System Resources,
 # Automate Directory Backups,
@@ -9,8 +10,13 @@ def get_sys_info():
     import platform
     import psutil
     print('Operating System: ' + platform.system() + ' v' + platform.version())
-    print('CPU Usages: ' + psutil.getloadavg())
-
+    print('CPU Usages: ' + str(psutil.cpu_percent(1)) + '%')
+    print('Total RAM avalible: ' + str(round(psutil.virtual_memory().total/1073741824)) + ' GB')
+    print('Used RAM: ' + str(round(psutil.virtual_memory().used/1073741824)) + ' GB')
+    print('Free RAM: ' + str(round(psutil.virtual_memory().free/1073741824)) + ' GB')
+    print('Used Diskspace: ' + str(psutil.disk_usage('/').percent) + '% ' + 'of ' + str(round(psutil.disk_usage('/').total/1073741824)) + ' GB')
+    print('Unused Diskspace: ' + str(round(psutil.disk_usage('/').free/1073741824)) + ' GB')
+    print('Python version: ' + platform.python_version())
 
 def main():
     get_sys_info()
